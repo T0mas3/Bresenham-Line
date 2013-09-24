2186d6c80902c4c64ecc1c3cb15dd4bcf957eee4
tomas <tomuibar@gmail.com>
2013-09-24 20:54:32 +0300

    A line can now be drawn by dragging mouse

--- main.cpp
+++ main.cpp
@@ -9,7 +9,27 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
+#include <iostream>
+using namespace std;
+
 #define GRID_SIZE 32
+
+int windowSizeH = 600;
+int windowSizeV = 600;
+
+bool drawLine = false;
+
+float beginX = 0;
+float beginY = 0;
+
+float endX = 1;
+float endY = 1;
+
+void transformScreenToWorldCoordinates(int screenX, int screenY, float &worldX, float &worldY) {
+
+	worldX = ((float)screenX / (float)windowSizeH) * 2 - 1;
+	worldY = ((float)screenY / (float)windowSizeV) * -2 + 1;
+}
 
 void colorGridCell(int x, int y) {
 
@@ -27,23 +47,48 @@ void colorGridCell(int x, int y) {
 		glVertex2f(topLeftX, topLeftY + cornerLength);
 	glEnd();
 
-//  Sample rect
-//	glVertex2f(-0.5, 0.5);
-//	glVertex2f(0.5, 0.5);
-//
-//	glVertex2f(0.5, -0.5);
-//	glVertex2f(-0.5, -0.5);
-
 }
 
-void display(void) {
-	int i;
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.5, 0.5, 0.5);
+void mouseClickCallback (int button, int state, int x, int y){
+
+	if (button == GLUT_LEFT_BUTTON){
+
+		if ((state == GLUT_DOWN)){
+			transformScreenToWorldCoordinates(x, y, beginX, beginY);
+			endX = beginX;
+			endY = beginY;
+			drawLine = true;
+
+		}
+		glutPostRedisplay();
+	}
+}
+
+void mouseDragCallback(int x, int y){
+
+	transformScreenToWorldCoordinates(x, y, endX, endY);
+
+	glutPostRedisplay();
+}
+
+void drawUserLine(){
+
+	glColor3f(1.0, 0.0, 0.0);
 
 	glBegin(GL_LINES);
-	for (i = -(GRID_SIZE/2); i <= (GRID_SIZE/2); i++) { //TODO optimize
-		float x = i / (float)(GRID_SIZE/2);
+		glVertex2f(beginX, beginY);
+		glVertex2f(endX, endY);
+	glEnd();
+}
+
+void drawGrid(){
+
+	glColor3f(0.5, 0.5, 0.5);
+	int i;
+
+	glBegin(GL_LINES);
+	for (i = -(GRID_SIZE / 2); i <= (GRID_SIZE / 2); i++) { //TODO optimize
+		float x = i / (float) (GRID_SIZE / 2);
 		glVertex2f(x, -1.0);
 		glVertex2f(x, 1.0);
 
@@ -51,11 +96,17 @@ void display(void) {
 		glVertex2f(1.0, x);
 	}
 	glEnd();
+}
 
-	colorGridCell(0, 0);
-	colorGridCell(31, 31);
-	colorGridCell(0, 31);
-	colorGridCell(31, 0);
+void display(void) {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	drawGrid();
+
+	if (drawLine) {
+		drawUserLine();
+	}
 
 	glFlush();
 }
@@ -66,11 +117,15 @@ void init(void) {
 int main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowSize(600, 600);
+	glutInitWindowSize(windowSizeH, windowSizeV);
 	glutInitWindowPosition(100, 50);
 	glutCreateWindow("CG1");
 	init();
 	glutDisplayFunc(display);
+
+	glutMouseFunc(mouseClickCallback);
+	glutMotionFunc(mouseDragCallback);
+
 	glutMainLoop();
 	return 0;
 }
